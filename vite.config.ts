cb154f644cc269d5610df00ef424f79fc2b20af3
Yo Iwamoto <you.iwamoto918@gmail.com>
2024-01-21 21:13:53 +0900

    fix: vite alias with dirname

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,17 @@
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@/": "src/",
+      "@": path.resolve(__dirname, "src"),
     },
   },
   server: {
