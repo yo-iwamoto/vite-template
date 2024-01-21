a309114ac989c9aad826b96b6dab8bc5f6142df5
Yo Iwamoto <you.iwamoto918@gmail.com>
2024-01-21 21:11:46 +0900

    fix: biome config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
 /** @type{import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.tsx'],
+  content: ["./src/**/*.tsx"],
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,22 @@
 /// <reference types="vitest" />
 
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@/': 'src/',
+      "@/": "src/",
     },
   },
   server: {
     open: true,
   },
   test: {
-    exclude: ['node_modules', './src/__tests__'],
+    exclude: ["node_modules", "./src/__tests__"],
     globals: true,
-    environment: 'jsdom',
-    setupFiles: ['src/__tests__/setup.ts'],
+    environment: "jsdom",
+    setupFiles: ["src/__tests__/setup.ts"],
   },
 });
